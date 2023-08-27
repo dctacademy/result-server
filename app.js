3df1a3ad3948cb6a282c16f9152dbc5f03b827ea
aniruddha <ani@dctacademy.com>
2023-08-28 00:08:21 +0530

    with home route

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 3044
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req,res) => {
+    res.json({
+        message: 'WELCOME TO Result Website'
+    })
+})
+
 app.listen(PORT, () => {
     console.log('server running on port ' + PORT)
 })
